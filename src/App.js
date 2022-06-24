a5fd85c45d43f21fc6daa8c920c7e70c2abe8be5
Rodolfo ceo cortes <ceosmarck@gmail.com>
2022-06-24 10:20:00 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Main from './componentes/Main';
+import './componentes/style.css'
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/Main.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Card from "./card";
+import Pokeinfo from "./poke_info";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+const Main = () => {
+    const [pokeData, setPokeData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+    const [initUrl, setInitUrl] = useState();
+    const [nextUrl, setNextUrl] = useState();
+    const [prevUrl, setPrevUrl] = useState();
+    const [pokeDex, setPokeDex] = useState();
+
+    const pokeFun = async () => {
+        setLoading(true)
+        const res = await axios.get(url);
+        setInitUrl(res.data.previous);
+        setNextUrl(res.data.next);
+        setPrevUrl(res.data.previous);
+        getPokemon(res.data.results)
+        setLoading(false)
+    }
+    const getPokemon = async (res) => {
+        res.map(async (item) => {
+            const result = await axios.get(item.url)
+            setPokeData(state => {
+                state = [...state, result.data]
+                state.sort((a, b) => a.id > b.id ? 1 : -1)
+                return state;
+            })
+        })
+    }
+    useEffect(() => {
+        pokeFun();
+    }, [url])
+    return (
+        <>
+            <div className="container">
+                <div className="left-content">
+                    <Card pokemon={pokeData} loading={loading} infoPokemon={poke => setPokeDex(poke)} />
+
+                    <div className="btn-group">
+
+                        {initUrl && <button onClick={() => {
+                            setPokeData([])
+                            setUrl(initUrl)
+                        }}>Inicio</button>}
+
+                        {prevUrl && <button onClick={() => {
+                            setPokeData([])
+                            setUrl(prevUrl)
+                        }}>Atrás</button>}
+
+                        {nextUrl && <button onClick={() => {
+                            setPokeData([])
+                            setUrl(nextUrl)
+                        }}>Siguiente</button>}
+                    </div>
+                </div>
+                <div className="right-content">
+                    <Pokeinfo data={pokeDex} />
+                </div>
+            </div>
+        </>
+    )
+}
+export default Main;
